4fa08872be16a4271f111f8cbc7896b973cef96a
Fedor Mukhin <62254476+mukhinfa@users.noreply.github.com>
2025-06-26 00:21:03 +0300

    Добавлен файл с реализацией параллельного вычисления квадратов случай… (#1)
    * Добавлен файл с реализацией параллельного вычисления квадратов случайных чисел с использованием горутин и каналов.
    
    * Добавлено начальное значение для генератора случайных чисел и исправлена ошибка в чтении из канала для вычисления квадратов.
    
    
    ---------
    
    Co-authored-by: f.mukhin <fmukhin@rotec.digital>

---
+++ 1-concurrency/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	chSlice := make(chan int)
+	chSqrs := make(chan int)
+	sl := make([]int, 0, 10)
+
+	go func() {
+		for range 10 {
+			sl = append(sl, rand.Intn(100))
+		}
+		for _, v := range sl {
+			chSlice <- v
+		}
+		close(chSlice)
+	}()
+	go func() {
+		for v := range chSlice {
+			chSqrs <- v * v
+		}
+		close(chSqrs)
+	}()
+	for {
+		select {
+		case p, ok := <-chSqrs:
+			if !ok {
+				return
+			}
+			fmt.Println(p)
+		default:
+			continue
+		}
+	}
+}
